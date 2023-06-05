acb193a1903539d2eaf746fee11e3699e827015b
alkina <alkina@mail.com>
2023-06-05 15:51:30 +0300

    created barcode scanner, changed index and 404

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,17 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
 import {RefresherCustomEvent} from '@ionic/angular';
 
 import {DataService, Medicine} from '../services/data.service';
 import {Subject, takeUntil} from "rxjs";
 
-// import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
+import {BarcodeScanner} from '@capacitor-community/barcode-scanner';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, OnDestroy {
   private data = inject(DataService);
   private medicines: Medicine[] = [];
 
@@ -47,22 +47,53 @@ export class HomePage implements OnInit {
 
   //Barcode
 
-  // async checkPermission()  {
-  //   try {
-  //     // check or request permission
-  //     const status = await BarcodeScanner.checkPermission({ force: true });
-  //
-  //     if (status.granted) {
-  //       // the user granted permission
-  //       return true;
-  //     }
-  //     return false;
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-  // };
+  async checkPermission() {
+    try {
+      // check or request permission
+      const status = await BarcodeScanner.checkPermission({force: true});
 
-  startScan() {
+      if (status.granted) {
+        // the user granted permission
+        return true;
+      }
+      return false;
+    } catch (e) {
+      console.log(e);
+      return e;
+    }
+  };
 
+  async startScan() {
+    try {
+      const permission = await this.checkPermission();
+      if (!permission) {
+        return;
+      }
+
+      await BarcodeScanner.hideBackground();
+      document?.querySelector('body')?.classList?.add('scanner-active');
+
+      const result = await BarcodeScanner.startScan(); // start scanning and wait for a result
+      console.log(result);
+
+      if (result?.hasContent) {
+        console.log(result.content); // log the raw scanned content
+        BarcodeScanner.showBackground();
+        document?.querySelector('body')?.classList?.remove('scanner-active');
+      }
+    } catch (e) {
+      console.log(e);
+      this.stopScan();
+    }
+  }
+
+  stopScan = () => {
+    BarcodeScanner.showBackground();
+    BarcodeScanner.stopScan();
+    document?.querySelector('body')?.classList?.remove('scanner-active');
+  };
+
+  ngOnDestroy(): void {
+    this.stopScan();
   }
 }
